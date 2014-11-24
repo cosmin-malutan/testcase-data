da8810a15ea8eb6706cac6a64faed9d3831f7aee
Cosmin Malutan <cosmin.malutan@softvision.ro>
2014-11-24 14:45:04 +0200

    mend

--- firefox/security/mixed_content_blocked/scripts/script1.js
+++ firefox/security/mixed_content_blocked/scripts/script1.js
@@ -1,4 +1,4 @@
-var r = document.getElementById("result1");
+var r = document.getElementById("result3");
 r.textContent = "LOADED";
 r.parentElement.className = "test fail";
 top.document.getElementById("frame").parentElement.className = "test fail";
